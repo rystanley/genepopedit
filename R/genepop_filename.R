76c2b526d4622459a312cb74d1697b16e742f0ac
bwringe <bwringe@mun.ca>
2016-05-26 08:29:17 -0300

    Added two internal functions to error check people's entries

---
+++ R/genepop_filename.R
@@ -0,0 +1,17 @@
+# Get File name specified by GenePop variable
+#' @title genepop_filename
+#' @description Gets the file name that is specified by GenePop variable
+#' @param X what to check
+#' @rdname genepop_filename
+#' @export
+#' @importFrom stringr str_split
+
+
+genepop_filename <- function(X){
+
+  GeneSplit <- unlist(stringr::str_split(string = GenePop, pattern = "/"))
+  GeneSplit <- GeneSplit[grep(x = GeneSplit, pattern = ".txt")]
+
+  return(GeneSplit)
+
+}
---
+++ R/path_ending.R
@@ -0,0 +1,16 @@
+# Check that file paths end in "/"
+#' @title path_ending
+#' @description Checks that the paths end in "/" as required
+#' @param path path to check
+#' @rdname path_ending
+#' @export
+#' @importFrom stringi stri_sub
+
+path_ending <- function(path){
+
+  if(stringi::stri_sub(path, -1) != "/"){path <- paste0(path, "/")}
+  if(stringi::stri_sub(path, -1) == "/"){path <- path}
+  quotes <-
+  writeLines("Note: File paths should end in a backslash (/) we have fixed this for you because we are nice")
+  return(path)
+}
