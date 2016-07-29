0cfae11f1f6cfaff237d9b9f31e857bf9591fa57
Thierry Gosselin <thierrygosselin@icloud.com>
2016-07-28 21:37:07 -0400

    * the dataset needs documentation...

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Documentation for the dataset used inside genepopedit
+#' This dataset contains an example of bla bla bla.
+
+#' @format A data frame with 411 rows and 1 variable: representing a genepop file with
+#' 100 Single Nucleotide Polymorphisms (SNPs).
+
+#' @details \itemize{
+#' \item Markers: 100 SNP
+#' \item Populations: 10
+#' \item Samples: 30 indivuals/population
+#' }
+#'
+#' @source bla bla bla.
+#'
+#' @keywords datasets
+"GenePopData"
+
+
