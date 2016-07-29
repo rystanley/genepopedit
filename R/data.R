7b22a9f8db4f32f1c2ccb4a954cd459875a880cb
Ryan Stanley <rstanley@mun.ca>
2016-07-29 09:09:40 -0300

    updated information

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
 #' Documentation for the dataset used inside genepopedit
-#' This dataset contains an example of bla bla bla.
+#' This dataset contains genomic data coded as 3 digit alleles.
 
 #' @format A data frame with 411 rows and 1 variable: representing a genepop file with
 #' 100 Single Nucleotide Polymorphisms (SNPs).
