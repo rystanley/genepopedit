d35c62e3d4bd58b5bd6ab90c6fe29ffa0011f1ad
Ryan Stanley <rstanley@mun.ca>
2016-08-03 17:03:12 -0300

    lowercase variables

--- R/pgdSpideR.R
+++ R/pgdSpideR.R
@@ -6,20 +6,20 @@
 #' @param output the full file path to the desired output. (e.g. GENEPOP or PED) these must match the dropdown menus in pgdSpider.
 #' @param output_format what are you converting to (e.g. GENEPOP or PED). This must match the dropdown menus in pgdSpider.
 #' @param spid the fill file path to the spid file generated in PGDspider.
-#' @param where.PGDspider A file path to the PGDspider installation folder.
+#' @param where.pgdspider A file path to the PGDspider installation folder.
 #' @rdname PGDspideR
 #' @importFrom stringr str_split
 #' @export
 
-PGDspideR <- function(input,input_format,output,output_format,spid,where.PGDspider){
+PGDspideR <- function(input,input_format,output,output_format,spid,where.pgdspider){
 
-where.PGDspider.PGD <- gsub(x = where.PGDspider, pattern = " ", replacement = "\\ ", fixed = TRUE)
+where.pgdspider.PGD <- gsub(x = where.pgdspider, pattern = " ", replacement = "\\ ", fixed = TRUE)
 
 ### Make sure the spid file required for the conversion is within the executable directory
-file.copy(from = spid, to = where.PGDspider, overwrite = TRUE)
+file.copy(from = spid, to = where.pgdspider, overwrite = TRUE)
 
 ## move the input file as well to the same location as PGDspider - this makes this step so much easier
-file.copy(from <- input, to = where.PGDspider, overwrite = TRUE)
+file.copy(from <- input, to = where.pgdspider, overwrite = TRUE)
 
 #File names
 input.name <- stringr::str_split(string = input, pattern = "/")
@@ -43,7 +43,7 @@ output.format <- paste("-outputformat",output_format,sep=" ")
 
 ## check the operating system
 if(Sys.info()["sysname"]=="Windows"){
-goto.spider <- paste0("cd ", where.PGDspider.PGD, " && ", execute.SPIDER)
+goto.spider <- paste0("cd ", where.pgdspider.PGD, " && ", execute.SPIDER)
 output.file.path <- paste("-outputfile", output.name, sep = " ")
 
 ## string to run
@@ -54,7 +54,7 @@ run.PGDspider <- paste0(goto.spider, " ", input.file.call, " ",
 ### run PGDspider through system
 shell(run.PGDspider)} else {
 
-    goto.spider <- paste0("cd ", where.PGDspider.PGD, "; ", execute.SPIDER)
+    goto.spider <- paste0("cd ", where.pgdspider.PGD, "; ", execute.SPIDER)
     output.file.path <- paste("-outputfile", output.name, sep = " ")
 
     ## string to run
@@ -69,12 +69,12 @@ shell(run.PGDspider)} else {
 
 
 ## move the converted file to the output pathPLINK folder
-file.copy(from = paste0(where.PGDspider,output.name), to = output, overwrite = TRUE)
+file.copy(from = paste0(where.pgdspider,output.name), to = output, overwrite = TRUE)
 
 ## remove the files you placed in the pgdSpider folder
-file.remove(paste0(where.PGDspider,input.name))
-file.remove(paste0(where.PGDspider,output.name))
-file.remove(paste0(where.PGDspider,spid.name))
+file.remove(paste0(where.pgdspider,input.name))
+file.remove(paste0(where.pgdspider,output.name))
+file.remove(paste0(where.pgdspider,spid.name))
 
 writeLines("Process Completed.")
 
