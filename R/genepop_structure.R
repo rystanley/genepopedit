abbaf4676567746e4a0a08d98c71ad6212b7cfac
Ryan Stanley <rstanley@mun.ca>
2016-08-03 16:37:21 -0300

    lowercase variables

--- R/genepop_structure.R
+++ R/genepop_structure.R
@@ -1,7 +1,7 @@
 # Genepop -> STRUCTURE
 #' @title Convert Genepop to STRUCTURE format.
 #' @description Function to convert Genepop to STRUCTURE
-#' @param GenePop the genepop data to be manipulated. This can be either a file path
+#' @param genepop the genepop data to be manipulated. This can be either a file path
 #' or a dataframe read in with tab separation, header=FALSE , quote="", and stringsAsFactors=FALSE.
 #' This will be the standard genepop format with the first n+1 rows corresponding to the n loci names,
 #' or a single comma delimited row of loci names followed by the locus data. Populations are
@@ -19,45 +19,45 @@
 #' @export
 
 
-genepop_structure <- function(GenePop,popgroup=NULL,path=NULL){
+genepop_structure <- function(genepop,popgroup=NULL,path=NULL){
 
-  #Check to see if GenePop is a data.frame from the workspace and convert to data.table
-  if(is.data.frame(GenePop)){GenePop <- as.data.table(GenePop)}
+  #Check to see if genepop is a data.frame from the workspace and convert to data.table
+  if(is.data.frame(genepop)){genepop <- as.data.table(genepop)}
 
-  #Check to see if Genepop is a file path or dataframe
-  if(is.character(GenePop)){
-    GenePop <- data.table::fread(GenePop,
+  #Check to see if genepop is a file path or dataframe
+  if(is.character(genepop)){
+    genepop <- data.table::fread(genepop,
                                  header = FALSE, sep = "\t",
                                  stringsAsFactors = FALSE)
   }
 
   ## check if loci names are read in as one large character vector (1 row)
-  header <- GenePop[1,]
+  header <- genepop[1,]
   if(length(gregexpr(',', header, fixed=F)[[1]])>1){
     lociheader <- strsplit(header,",")
     lociheader <- gsub(" ","",unlist(lociheader))
     #remove the first column of loci names
-    GenePop <- as.vector(GenePop)
-    GenePop <- GenePop[-1,]
-    GenePop <- c(lociheader,GenePop)
-    GenePop <- as.data.table(GenePop,stringsAsFactors = FALSE)
+    genepop <- as.vector(genepop)
+    genepop <- genepop[-1,]
+    genepop <- c(lociheader,genepop)
+    genepop <- as.data.table(genepop,stringsAsFactors = FALSE)
   }
 
   ## Stacks version information
-  stacks.version <- GenePop[1,] #this could be blank or any other source. First row is ignored by GenePop
+  stacks.version <- genepop[1,] #this could be blank or any other source. First row is ignored by genepop
 
   #Remove first label of the stacks version
-  GenePop <- GenePop[-1,]
-  colnames(GenePop) <- "data"
+  genepop <- genepop[-1,]
+  colnames(genepop) <- "data"
 
   #ID the rows which flag the Populations
-  Pops  <-  which(GenePop$data == "Pop" | GenePop$data =="pop" | GenePop$data == "POP")
+  Pops  <-  which(genepop$data == "Pop" | genepop$data =="pop" | genepop$data == "POP")
   npops  <-  1:length(Pops)
 
   ## separate the data into the column headers and the rest
-  ColumnData <- GenePop$data[1:(Pops[1]-1)]
+  ColumnData <- genepop$data[1:(Pops[1]-1)]
   ColumnData <- gsub("\r","",ColumnData)#remove any hidden carriage returns
-  snpData <- GenePop[Pops[1]:NROW(GenePop),]
+  snpData <- genepop[Pops[1]:NROW(genepop),]
 
   #Get a datafile with just the snp data no pops
   tempPops <- which(snpData$data=="Pop"| snpData$data =="pop" | snpData$data == "POP") ## Changed because we allowed
